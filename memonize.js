68b57009791faa3433940f463518a2ef6cc50c47
Jayabalakrishnan <rjayabalakrishna@lm-ast-21021629.paypalcorp.com>
2019-03-27 09:47:11 -0500

    memonize code block

---
+++ memonize.js
@@ -0,0 +1,17 @@
+function memonize(fn) {
+  const cache = {};
+  return function(...args){
+    if (cache[args]){
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+module.exports = {
+  memonize
+};
